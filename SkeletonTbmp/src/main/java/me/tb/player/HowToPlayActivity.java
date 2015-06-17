06835ca30db4cb8a5ed5cabee1f4d7e50fdb3b69
Justin <juhess88@gmail.com>
2015-06-16 21:51:56 -0400

    Trying to add swipe tab views
    TODO - change recyclerview text at end of game, swipe tabs for rules, add tiles counter to game view and possibly enter button

--- SkeletonTbmp/src/main/java/me/tb/player/HowToPlayActivity.java
+++ SkeletonTbmp/src/main/java/me/tb/player/HowToPlayActivity.java
@@ -7,11 +7,12 @@ import android.support.v7.app.ActionBarActivity;
  * Created by Juhess88 on 6/14/2015.
  */
 public class HowToPlayActivity extends ActionBarActivity {
-
-    @Override
+ @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_how_to_play);
+
+
     }
 }
